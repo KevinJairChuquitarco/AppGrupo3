8776489346178634e326caf0ba2e5b64461bba8e
KevinJairChuquitarco <88475868+KevinJairChuquitarco@users.noreply.github.com>
2022-06-28 09:59:11 -0500

    Cambios Seguros

--- includes/db.php
+++ includes/db.php
@@ -19,7 +19,6 @@ class DB{
             
             $connection = "mysql:host=" . $this->host . ";dbname=" . $this->db;
             $pdo = new PDO($connection, $this->user, $this->password);
-    
             return $pdo;
 
         }catch(PDOException $e){
