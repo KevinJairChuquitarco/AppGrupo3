f9156bece09bfa26474a87539ff8eb5a26dd977f
KevinJairChuquitarco <88475868+KevinJairChuquitarco@users.noreply.github.com>
2022-06-28 11:32:48 -0500

    Seguridad

--- index.php
+++ index.php
@@ -12,24 +12,18 @@
     $user = new User();
 
     if(isset($_SESSION['user'])){
-        //echo "Hay sesión";
     $user->setUser($userSession->getCurrentUser());
     include_once 'vistas/home.php';
 }else if(isset($_POST['username']) && isset($_POST['password'])){
-    //echo "Validación de login";
-
     $userForm = $_POST['username'];
     $passForm = $_POST['password'];
 
     if($user->userExists($userForm, $passForm)){
-        //echo "usuario validado";
         $userSession->setCurrentUser($userForm);
         $user->setUser($userForm);
-
         include_once 'vistas/home.php';
     }else{
-        //echo "nombre de usuario y/o password incorrecto";
-        $errorLogin = "Nombre de usuario y/o password es incorrecto";
+        $errorLogin = "Nombre de Usuario y/o Contraseña no es correcto";
         include_once 'vistas/login.php';
     }
 
